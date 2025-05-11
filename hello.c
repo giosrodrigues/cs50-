327959ba5199972a8ac31e39ddec7546a5b9da49
giovanna <giovannarodrigues17066@gmail.com>
2025-05-11 20:15:29 -0300

    Adicionando hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world!\n");
+}
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string giovanna = get_string("giovanna \n");
+    printf("hello, %s \n", giovanna);
+}
